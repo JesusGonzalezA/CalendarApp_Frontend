e975cd06d7ad3af4f4fba9e2fa845fa88277db1c
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-01-21 11:29:34 +0100

    Add calendar screen with styles

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,37 @@
 import React from 'react'
+
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
+
 import { Navbar } from '../ui/Navbar'
+
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import '../../styles.css'
+//**************************************************************************
+
+const localizer = momentLocalizer(moment);
+
+const events = [{
+    title: 'Hacer ejercicio',
+    start: moment().toDate(),
+    end: moment().add( 2, 'days' ).toDate(),
+    bgcolor: '#fafafa'
+}]
 
 export const CalendarScreen = () => {
     return (
-        <div>
-            <Navbar />
+        <div className="calendar-screen">
+            <Navbar className="navbar"/>
+
+            <div className="calendar-container">
+                <Calendar
+                    className="calendar"
+                    localizer={ localizer }
+                    events={ events }
+                    startAccessor="start"
+                    endAccessor="end"
+                />
+            </div>
         </div>
     )
 }

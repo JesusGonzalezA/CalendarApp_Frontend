f77a7367eb7ba80762471116a2e1ea6b560beff1
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-02-03 19:11:09 +0100

    Add loading component

---
+++ src/components/ui/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './loading.css'
+
+export const Loading = () => {
+    return (
+        <div class="loading__container">
+            <h1 class="loading__title">
+                Loading...
+            </h1>
+        <div class="loading__loader"></div>
+    </div>
+    )
+}

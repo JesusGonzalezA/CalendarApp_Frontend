a78f98ebb979b3480a70bbb1189bb53d4b2c08c8
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-02-03 12:14:27 +0100

    Add auth to store

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 import { eventsReducer } from './eventsReducer'
+import { authReducer } from './authReducer'
 
 import { uiReducer } from './uiReducer'
 //**************************************************************************
 
 export const rootReducer = combineReducers({
     ui: uiReducer,
-    events: eventsReducer
+    events: eventsReducer,
+    auth: authReducer
 })
 
 
--- src/types/types.js
+++ src/types/types.js
@@ -8,5 +8,12 @@ export const types = {
     eventsSetActive: '[events] Set active,',
     eventsClearActive: '[events] Clear active',
     eventsUpdate: '[events] Event update',
-    eventsDelete: '[events] Event delete'
+    eventsDelete: '[events] Event delete',
+
+    authChecking: '[auth] Checking login state',
+    authCheckingFinish: '[auth] Finish Checking login state',
+    authStartLogin: '[auth] Start login',
+    authStartRegister: '[auth] Start register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogout: '[auth] Logout'
 }

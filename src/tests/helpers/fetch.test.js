3ce0e3650a048aef714ab8fe77b5eb4f97f3fdae
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-02-11 10:14:32 +0100

    Testing fetch with token

--- src/tests/helpers/fetch.test.js
+++ src/tests/helpers/fetch.test.js
@@ -1,17 +1,66 @@
-import { fetchWithoutToken } from "../../helpers/fetch";
+import { fetchWithoutToken, fetchWithToken } from "../../helpers/fetch";
 
 describe('Testing fetch helper', () => {
 
+    const user = {
+        email: 'test@test.test',
+        password: 'testtest'
+    };
+
+    let token = '';
+
     test('FetchWithoutToken should work', async () => {
         
-        const resp = await fetchWithoutToken('auth', {
-            email: 'test@test.test',
-            password: 'testtest'
-        }, 'POST');
+        const resp = await fetchWithoutToken('auth', user, 'POST');
         expect( resp instanceof Response ).toBe( true );
 
         const body = await resp.json();
-        expect( body.ok ).toBe( false );
-    })
+        expect( body.ok ).toBe( true );
+
+        token = body.data.token;
+    });
+
+    test('FetchWithToken should work with the correct token', async () => {
+        
+        localStorage.setItem('token', token);
+
+        const resp = await fetchWithToken('events');
+        expect( resp instanceof Response ).toBe( true );
+
+        const body = await resp.json();
+        expect( body ).toEqual( {
+            ok: true,
+            data: expect.any(Array)
+        });
+    });
+
+
+    test('FetchWithToken should work with an empty token', async () => {
+        localStorage.removeItem('token');
+
+        const resp = await fetchWithToken('events');
+        expect( resp instanceof Response ).toBe( true );
+
+        const body = await resp.json();
+        expect( body ).toEqual( {
+            "ok": false,
+            "msg": "Token is required"
+        });
+    });
+
+    test('FetchWithToken should work with an empty token', async () => {
+
+        localStorage.setItem('token', '  t')
+
+        const resp = await fetchWithToken('events');
+        expect( resp instanceof Response ).toBe( true );
+
+        const body = await resp.json();
+        expect( body ).toEqual( {
+            "ok": false,
+            "msg": "Token is not valid"
+        });
+    });
+    
     
 })
